a924275424a457beb0637218cf4e3bb50f3ba1ea
Taha <itaha@live.com>
2025-02-26 12:03:29 +0000

    added the function app code

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+  
+}
+
+resource "azurerm_resource_group" "beawartrg" {
+  name = "beawart-resources"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "beawartstorage" {
+  name = "beawartstorageacc"
+  resource_group_name = azurerm_resource_group.beawartrg.name
+  location = azurerm_resource_group.beawartrg.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "bewartasp" {
+  name = "beawart-asp"
+  resource_group_name = azurerm_resource_group.beawartrg.name
+  location = azurerm_resource_group.beawartrg.location
+  sku_name = "F1"
+  os_type = "Linux"
+}
