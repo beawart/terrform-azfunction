fd893b532cb429a898a9a6f755c5ccbbe77d3597
Taha <itaha@live.com>
2025-02-27 06:01:37 +0000

    commented the storage account provisioning

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "azurerm" {
-  
+  features {
+  }
+  subscription_id = "3ff211f1-4d5d-4ed8-b3fc-858378f20b7d"
 }
 
 resource "azurerm_resource_group" "beawartrg" {
@@ -7,18 +9,18 @@ resource "azurerm_resource_group" "beawartrg" {
   location = "East US"
 }
 
-resource "azurerm_storage_account" "beawartstorage" {
-  name = "beawartstorageacc"
-  resource_group_name = azurerm_resource_group.beawartrg.name
-  location = azurerm_resource_group.beawartrg.location
-  account_tier = "Standard"
-  account_replication_type = "LRS"
-}
+# resource "azurerm_storage_account" "beawartstorage" {
+#   name = "beawartstorageacc"
+#   resource_group_name = azurerm_resource_group.beawartrg.name
+#   location = azurerm_resource_group.beawartrg.location
+#   account_tier = "Standard"
+#   account_replication_type = "LRS"
+# }
 
-resource "azurerm_service_plan" "bewartasp" {
-  name = "beawart-asp"
-  resource_group_name = azurerm_resource_group.beawartrg.name
-  location = azurerm_resource_group.beawartrg.location
-  sku_name = "F1"
-  os_type = "Linux"
-}
+# resource "azurerm_service_plan" "bewartasp" {
+#   name = "beawart-asp"
+#   resource_group_name = azurerm_resource_group.beawartrg.name
+#   location = azurerm_resource_group.beawartrg.location
+#   sku_name = "F1"
+#   os_type = "Linux"
+# }
